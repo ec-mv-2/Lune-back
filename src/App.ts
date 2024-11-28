b357d37ae185e611decd343e4d886accc9496a38
Ohana Luz <123988706+ohanaluz@users.noreply.github.com>
2024-11-28 02:12:58 -0300

    Add files via upload

--- src/App.ts
+++ src/App.ts
@@ -10,7 +10,7 @@ const app = express()
 const server = http.createServer(app);
 
 
-mongoose.connect('mongodb://localhost:27017/lune');
+mongoose.connect('mongodb://127.0.0.1:27017/lune');
 
 const io = new Server(server, {
     cors: {
